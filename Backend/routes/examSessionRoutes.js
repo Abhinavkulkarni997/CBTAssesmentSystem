8615349cfa26880878222f57ba5e397dd3b16a9c
unknown <Abhinavkulkarni997>
2026-06-03 15:28:34 +0530

    Backend completed up to exam session

--- Backend/routes/examSessionRoutes.js
+++ Backend/routes/examSessionRoutes.js
@@ -2,10 +2,11 @@ const express=require("express");
 const router=express.Router();
 const{startExam,getCurrentSession,saveAnswer,submitExam,getResults}=require("../controllers/examSessionController");
 const {protect}=require("../middleware/authMiddleware");
+const {adminOnly}=require("../middleware/adminMiddleware");
 
 router.post("/start",protect,startExam);
 router.get("/current",protect,getCurrentSession);
 router.post("/save-answer",protect,saveAnswer);
 router.post("/submit",protect,submitExam);
-router.get("/results",protect,getResults);
+router.get("/results",protect,adminOnly,getResults);
 module.exports=router;
